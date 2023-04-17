97d4b37583caf7762e19eaa3fd68445483955cb9
streeese <94462388+streeese@users.noreply.github.com>
2023-04-17 18:43:19 -0400

    Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Comuna, Direccion, Producto, Compra, Persona, Cliente
 
 # Register your models here.
+
+admin.site.register(Comuna)
+admin.site.register(Direccion)
+admin.site.register(Producto)
+admin.site.register(Compra)
+admin.site.register(Persona)
+admin.site.register(Cliente)
